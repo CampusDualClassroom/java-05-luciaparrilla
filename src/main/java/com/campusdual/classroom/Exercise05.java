5630ab53615c6b83cd78a644df3e007f8941cc8f
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-10-14 17:27:31 +0200

    Terminado 05

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -2,6 +2,16 @@ package com.campusdual.classroom;
 
 public class Exercise05 {
     public static void main(String[] args) {
+        checkLessOrGreaterEqual(9, 18);
+        checkLessOrGreaterEqual(18, 9);
+        checkLessOrGreaterEqual(9,9);
+
+        checkMultiply(9,3);
+        checkMultiply(9,19);
+
+        checkPositiveNegative(9);
+        checkPositiveNegative(-9);
+        checkPositiveNegative(0);
 
     }
 
@@ -9,13 +19,22 @@ public class Exercise05 {
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
-
+        if (n1 < n2) {
+            System.out.println(n1 + " es menor que " + n2);
+        } else{
+            System.out.println(n1 + " es mayor o igual que " + n2);
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
+        if (n2 != 0 && n1 % n2 == 0) {
+            System.out.println(n1 + " es múltiplo de " + n2);
+        } else {
+            System.out.println(n1 + " no es múltiplo de " + n2);
+        }
 
     }
 
@@ -23,6 +42,10 @@ public class Exercise05 {
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
-        
+        if (num < 0) {
+            System.out.println(num + " es negativo.");
+        } else {
+            System.out.println(num + " es positivo.");
+        }
     }
 }
